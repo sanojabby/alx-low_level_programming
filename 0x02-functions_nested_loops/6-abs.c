d4bb140a6ca69474aea625d1c2fe0ba3193feace
sanojabby <sanobaptiste2018@gmail.com>
2022-06-08 19:43:44 -0700

    nested

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - to ckech the absolute value
+ * @a: is a data type
+ * Return: absolute value of integer
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+	return (-a);
+	}
+	else
+	{
+		return (a);
+
+	}
+}
