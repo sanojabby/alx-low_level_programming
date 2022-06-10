bcc37ff22dc0bfe4b895298bb7a3da44628b3304
sanojabby <sanobaptiste2018@gmail.com>
2022-06-09 17:08:30 -0700

    print integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *print_number - function that prints an integer..
+ * print_number - function that prints an integer..
+ *
  * @n: input value to check
  * Return: nothing.
  */
-
 void print_number(int n)
 {
 unsigned int num, num2;
