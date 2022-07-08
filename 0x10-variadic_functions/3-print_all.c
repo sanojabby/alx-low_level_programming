5a96e3fc78e6a6afe57ad6e17455c52165608cde
sanojabby <sanobaptiste2018@gmail.com>
2022-07-08 03:38:29 -0700

    function that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,49 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "variadic_functions.h"
 
 /**
- * print_all - prints anything.
- * @format: a list of types of arguments passed to the function.
- *
- * Return: no return.
+ * print_i - prints int
+ * @list: arguement of list
+ * @s: seperator
+ * Return: none
  */
+
+void print_i(va_list list, char *s)
+{
+printf("%s%d", s, va_arg(list, int));
+}
+
+/**
+ * print_c - prints char
+ *  @list: arguement char
+ *  @sep: seperator
+ */
+
+void print_c(va_list list, char *sep)
+{
+printf("%s%c", sep, va_arg(list, int));
+}
+
+/**
+ * print_s - prints string
+ *  @sep: seperator
+ *  list: list to print
+ *  Return: none
+ */
+
+void print_s(va_list list, char *sep)
+{
+char *s;
+
+s = va_arg(list, char *);
+if (s == NULL)
+s = "(nil)";
+printf("%s%s", sep, s);
+}
+
+/**
+ * print_f - prints floats
+ * @sep: float to print
+ * @list: next arguement of list to print
+ * Return: none
+ */
+
+void print_f(va_list list, char *sep)
+{
+printf("%s%f", sep, va_arg(list, double));
+}
+
+/**
+ * print_all -  function that prints anything
+ * @format:  list of types of arguments passed to the function
+ * Return: nothing
+ */
+
 void print_all(const char * const format, ...)
 {
-va_list valist;
-unsigned int i = 0, j, c = 0;
-char *str;
-const char t_arg[] = "cifs";
+va_list list;
+int i, j;
+char *separator;
 
-va_start(valist, format);
-while (format && format[i])
+type_t ops[] = {
+	{"c", print_c},
+	{"i", print_i},
+	{"f", print_f},
+	{"s", print_s},
+	{NULL, NULL}
+};
+
+va_start(list, format);
+i = 0;
+separator = "";
+while (format != NULL && format[i] != '\0')
 {
 j = 0;
-while (t_arg[j])
+while (j < 4)
 {
-printf(", ");
-break;
-} j++;
-}
-switch (format[i])
+if (format[i] == *(ops[j]).op)
 {
-case 'c':
-printf("%c", va_arg(valist, int)), c = 1;
-break;
-case 'i':
-printf("%d", va_arg(valist, int)), c = 1;
-break;
-case 'f':
-printf("%f", va_arg(valist, double)), c = 1;
-break;
-case 's':
-str = va_arg(valist, char *), c = 1;
-if (!str)
-{
-printf("(nil)");
-break;
+ops[j].f(list, separator);
+separator = ", ";
+
 }
-printf("%s", str);
-break;
-} i++;
+j++;
 }
-printf("\n"), va_end(valist);
+i++;
+}
+printf("\n");
+va_end(list);
 }
