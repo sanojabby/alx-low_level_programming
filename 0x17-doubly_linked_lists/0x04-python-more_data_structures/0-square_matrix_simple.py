555952c64e41d4f8f37ad28a6adf75015ad6221c
sanojabby <sanobaptiste2018@gmail.com>
2022-09-01 03:37:03 -0700

    0-square_matrix_simple added

--- 0x17-doubly_linked_lists/0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x17-doubly_linked_lists/0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-
-    if len(matrix) > 0:
-        for elems in matrix[:]:
-            new_matrix.append(list(map(lambda x: x ** 2, elems)))
-
-            return new_matrix
+    squared = []
+    for line in matrix:
+        squared.append([c**2 for c in line])
+        return squared
